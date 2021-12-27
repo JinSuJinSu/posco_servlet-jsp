95c998822c4e4c0bbcc829afaf037de510b0afcb
JinsuJinsu <hjs928.naver.com>
2021-12-27 18:46:20 +0900

    add servlet datas

---
+++ edu/src/main/java/FirstServlet.java
@@ -0,0 +1,33 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+@WebServlet("/first")
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.print("<h1>FirstServlet 수행 완료</h1>");
+		out.print("<hr>");
+		out.print("<h2>요청 방식 : " + request.getMethod() + "</h2>");
+		out.print("<h2>반갑다... " + request.getParameter("name") + "님</h2>");
+		System.out.println("서블릿에서의 표준 출력은 어디로 나갈까");
+		
+	}
+
+}
